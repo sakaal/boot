0fe8a58a10cb7d9a95ffc93e759c93d96fce66f5
Sakari Maaranen <sam@iki.fi>
2013-08-15 18:42:21 +0300

    Use spice graphics for the example.

--- root/virt-install_boot.local.sh
+++ root/virt-install_boot.local.sh
@@ -7,7 +7,7 @@ ssh-keygen -R 192.168.122.8
 virt-install --connect=qemu:///system \
 --network=network:default,model=virtio \
 --initrd-inject ~/git/boot/root/boot.local-ks.cfg \
---extra-args="ks=file:/boot.local-ks.cfg console=tty0 console=xvc0,115200n8 serial" \
+--extra-args="ks=file:/boot.local-ks.cfg console=tty0 console=xvc0,115200n8" \
 --name=boot.local \
 --description="A bootstrapping node" \
 --disk path=/dev/vg_guests/lv_boot.local,size=4,bus=virtio,sparse=false,cache=writethrough \
@@ -20,5 +20,5 @@ virt-install --connect=qemu:///system \
 --accelerate \
 --virt-type=kvm \
 --location=http://www.nic.funet.fi/pub/Linux/INSTALL/Centos/6.4/os/x86_64/ \
---graphics=none \
+--graphics=spice \
 --noautoconsole
